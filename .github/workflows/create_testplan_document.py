c19ba97fd7d1bc8ed9b9d3c82ac93e1358590ba7
Tomas Levin <tomas.levin@cacanny.net>
2023-10-19 22:29:04 +0200

    Added timestamp to ensure generation of new file

--- .github/workflows/create_testplan_document.py
+++ .github/workflows/create_testplan_document.py
@@ -1,7 +1,13 @@
+import datetime
+
 def run():
+    current_time = datetime.datetime.now()
+
     with open("testplan.md","w") as markdown_testplan:
         markdown_testplan.write("# This is a test\n")
         markdown_testplan.write("This is a test markdown file for testing\n")
+        markdown_testplan.write(current_time)
+
 
 if __name__ == '__main__':
     run()
