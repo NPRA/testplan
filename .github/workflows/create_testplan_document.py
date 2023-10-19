4f1e091333f2d44e0cd9b842daa8987d0d2204de
Tomas Levin <tomas.levin@cacanny.net>
2023-10-19 23:01:44 +0200

    Test using mdutils module

--- .github/workflows/create_testplan_document.py
+++ .github/workflows/create_testplan_document.py
@@ -1,14 +1,20 @@
 import datetime
+import mdutils
 
 def run():
     current_time = str(datetime.datetime.now())
 
-    with open("testplan.md","w") as markdown_testplan:
-        markdown_testplan.write("# This is a test\n")
-        markdown_testplan.write("![NPRA logo](/graphics/logo NPRA.jpg "Norwegian Public Roads Administration")")
-        markdown_testplan.write("This is a test markdown file for testing\n")
-        markdown_testplan.write(current_time)
+    mdFile = mdutils.MdUtils(file_name='testplan.md',title='Markdown of testplan for Jammertest')
+    mdFile.new_header(level=1, title='Jammertest 2024 Testplan')
+    mdFile.new_inline_image("NPRA logo", "/graphics/logo NPRA.jpg")
 
+    mdFile.new_paragraph(""" This is a test to add a significant block of text
+                         and this is the second line 
+                         and third line
+                         """)
+    mdFile.new_table_of_contents(table_title='Contents', depth=2)
+    mdFile.create_md_file()
 
+ 
 if __name__ == '__main__':
     run()
