7092adbb5f71aba0ca0cbd77826a357712ab6d3b
Tomas Levin <tomas.levin@cacanny.net>
2023-10-19 22:38:29 +0200

    Test with NPRA logo

--- .github/workflows/create_testplan_document.py
+++ .github/workflows/create_testplan_document.py
@@ -5,6 +5,7 @@ def run():
 
     with open("testplan.md","w") as markdown_testplan:
         markdown_testplan.write("# This is a test\n")
+        markdown_testplan.write("![NPRA logo](/graphics/logo NPRA.jpg "Norwegian Public Roads Administration")")
         markdown_testplan.write("This is a test markdown file for testing\n")
         markdown_testplan.write(current_time)
 
